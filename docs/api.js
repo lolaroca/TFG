6b362f71dc65b400eaf9a15a8c0e7222b9f9893c
lolar <lolarocamorlan@hotmail.com>
2022-12-03 21:37:19 +0100

    cambios

--- docs/api.js
+++ docs/api.js
@@ -3,16 +3,28 @@ url = "https://api.chucknorris.io/jokes/random?category=";
 const getChiste = async () => {
     //const image = document.createElement('img');
 
-    categoria = document.querySelector('#categorias');
+    categoria = document.querySelector('#categorias'); //coge id=categorias
     valor = categoria.value;
     link = url + valor;
 	let request = await fetch(link);
 	if (request.status === 200) {
 		let data = await request.json();
 		console.log(data);
-	    document.getElementById("chiste").innerHTML = data.value;
+	    document.getElementById("chiste").innerHTML = data.value; //instertamos el data en chiste
 		document.getElementById("foto").innerHTML = data.icon_url;
 	}
 };
+
+const getCredenciales = async () => {
+	url=https://api.energomonitor.com/v1/users/usoxx
+	let request = await fetch(link);
+	if (request.status === 200) {
+		let data = await request.json();
+		console.log(data);
+	    document.getElementById("email").innerHTML = data.email; //insertamos el email en 
+	}
+}
+
+
 
 getChiste();
