ea34629aef114129abba60676a3afed74eb7b764
lolar <lolarocamorlan@hotmail.com>
2023-02-12 12:07:42 +0100

    cambios

--- docs/api.js
+++ docs/api.js
@@ -1,44 +1,53 @@
-//urlprueba = "https://api.chucknorris.io/jokes/random?category=";
-
-/*const getChiste = async () => {
-    //const image = document.createElement('img');
-
-    categoria = document.querySelector('#categorias'); //coge id=categorias
-    valor = categoria.value;//cogemos animal
-    link = urlprueba + valor;
-	let request = await fetch(link);
-	if (request.status === 200) {
-		let data = await request.json();
-		console.log(data);
-	    document.getElementById("chiste").innerHTML = data.value; //instertamos el data en chiste
-		document.getElementById("foto").innerHTML = data.icon_url;
-	}
-};*/
-
-
 
 let i=0;
 const getCredenciales = async () => {
 	datoDoc=document.querySelector('#sensores');
 	dato=datoDoc.value;
-	//dato="emjeor";
-	link="https://api.energomonitor.com/v1/feeds/emjeic/streams/"+dato+"/data?limit=10";
+
+	//tiempo inicial
+	tiempoInicioDoc=document.getElementById("inicio");
+	tiempoI=tiempoInicioDoc.value;
+	for (var i=0;i<11;i++){
+		var dateString = tiempoI;
+		var date = new Date(dateString);
+		var timestampInicio= (date.getTime()/1000);
+	}
+	tiempoInicio=timestampInicio;
+	
+
+	//tiempo final
+	tiempoFinalDoc=document.getElementById("final");
+	tiempoF=tiempoFinalDoc.value;
+	for (var i=0;i<11;i++){
+		var dateString = tiempoF;
+		var date = new Date(dateString);
+		var timestampFinal= (date.getTime()/1000);
+	}
+	tiempoFinal=timestampFinal;
+
+	
+	link="https://api.energomonitor.com/v1/feeds/emjeic/streams/"+dato+"/data?data_time_from="+tiempoInicio+"&data_time_to="+tiempoFinal;
 	console.log(link);
 
 	let request = await fetch(link, {
 		method: 'GET',
 		headers: new Headers({
-			'Authorization': 'Bearer '+ '8Hbf70l1NF0L9wA1whSFzRZq1V3wxR'
+			'Authorization': 'Bearer '+ '0tawywjEOw9rmaF2y49dFefePwa4Ya'
 		})
 	});
 	if (request.status === 200) {
 		let data = await request.json();
+		var table = document.getElementById("table");
 		var timestamp = data[i][0];
 		var date = new Date(timestamp*1000);
-		var fecha ="Fecha: "+date.getDate()+ "/"+(date.getMonth()+1)+"/"+date.getFullYear()+" "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
-		var dato = "Dato[Wh]: "+ data[i][1];
-		document.getElementById("tiempo").innerHTML = fecha; 
-	    document.getElementById("dato").innerHTML = dato;
+		var fecha =date.getDate()+ "/"+(date.getMonth()+1)+"/"+date.getFullYear()
+		var hora=date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+		var datoFinal = data[i][1];
+
+		document.getElementById("fecha").innerHTML = fecha; 
+		document.getElementById("hora").innerHTML = hora; 
+	    document.getElementById("dato").innerHTML = datoFinal;
+
 		
 	}
 	i=i+1;
@@ -47,8 +56,5 @@ const getCredenciales = async () => {
 	}
 }
 
-
-
-
-//getChiste();
 getCredenciales();
+
